d20be4f7813f22e041244325d173d528a6879f73
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-09-20 14:41:10 +0300

    created docker-compose for postregs and fastapi containers

---
+++ wait_for_db.py
@@ -0,0 +1,34 @@
+import os
+import asyncio
+import asyncpg
+
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+async def wait_for_db():
+    while True:
+        try:
+            # Retrieve database connection parameters from environment variables
+            dbname = os.getenv('POSTGRES_DB')
+            user = os.getenv('POSTGRES_USER')
+            password = os.getenv('POSTGRES_PASSWORD')
+            host = 'db'  # Service name in Docker Compose
+            port = '5432'
+            # Attempt to connect to the database
+            conn = await asyncpg.connect(
+                user=user,
+                password=password,
+                database=dbname,
+                host=host,
+                port=port
+            )
+            await conn.close()
+            break
+        except (asyncpg.ConnectionError, asyncpg.InvalidPasswordError) as e:
+            print("Waiting for PostgreSQL to be ready...")
+            await asyncio.sleep(2)
+
+if __name__ == "__main__":
+    asyncio.run(wait_for_db())
